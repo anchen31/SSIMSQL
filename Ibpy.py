bfc40c73f7bdf7150fc00da9dcac2b449e11b475
Andy Chen <andy.chen69@outlook.com>
2021-08-16 09:15:33 -0700

    Update Ibpy.py

--- Ibpy.py
+++ Ibpy.py
@@ -13,6 +13,11 @@ ib.connect('127.0.0.1', 7497, clientId=2)
 
 sia = SentimentIntensityAnalyzer()
 
+def datetime_from_utc_to_local(utc_datetime):
+    now_timestamp = time.time()
+    offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp)
+    return utc_datetime + offset
+
 contract1 = Stock('SPY', 'SMART', 'USD')
 
 #1 pull data from here
@@ -69,15 +74,15 @@ contract1 = Stock('SPY', 'SMART', 'USD')
 
 
 
-# plt.rcParams['figure.figsize'] = [12, 7]
-# plt.rc('font', size=14)
+plt.rcParams['figure.figsize'] = [12, 7]
+plt.rc('font', size=14)
 
-# name = 'SPY'
-# ticker = yfinance.Ticker(name)
-# df1 = ticker.history(interval="5m",start="2021-08-5",end="2021-08-6", threads= False)
-# df1['Date'] = pd.to_datetime(df1.index)
-# df1['Date'] = df1['Date'].apply(mpl_dates.date2num)
-# df1 = df1.loc[:,['Date', 'Open', 'High', 'Low', 'Close']]
+name = 'SPY'
+ticker = yfinance.Ticker(name)
+df1 = ticker.history(interval="5m",start="2021-08-12",end="2021-08-13", threads= False)
+df1['Date'] = pd.to_datetime(df1.index)
+df1['Date'] = df1['Date'].apply(mpl_dates.date2num)
+df1 = df1.loc[:,['Date', 'Open', 'High', 'Low', 'Close']]
 
 
 
@@ -89,9 +94,13 @@ newsProviders = ib.reqNewsProviders()
 codes = '+'.join(np.code for np in newsProviders)
 
 ib.qualifyContracts(contract1)
-headlines = ib.reqHistoricalNews(contract1.conId, codes, '2021-08-5', '2021-08-6', 50)
+# headlines = ib.reqHistoricalNews(contract1.conId, codes, '2021-08-12', '2021-08-13', 50)
+headlines = ib.reqHistoricalNews(contract1.conId, codes, '', '', 50)
+
 for i in headlines:
     latest = i.headline
+    # time = i.time
+    # time = datetime_from_utc_to_local(time)
     vs = sia.polarity_scores(latest)
     sentiment = round(vs['compound'], 4)
     time = i.time
@@ -102,21 +111,21 @@ for i in headlines:
 
 
 
-# fig, ax = plt.subplots()
-# candlestick_ohlc(ax,df1.values,width=0.0001, \
-#                colorup='green', colordown='red', alpha=0.8)
-# date_format = mpl_dates.DateFormatter('%Y-%m-%d %H:%M:%S')
-# ax.xaxis.set_major_formatter(date_format)
-# fig.autofmt_xdate()
-# fig.tight_layout()
+fig, ax = plt.subplots()
+candlestick_ohlc(ax,df1.values,width=0.0001, \
+               colorup='green', colordown='red', alpha=0.8)
+date_format = mpl_dates.DateFormatter('%Y-%m-%d %H:%M:%S')
+ax.xaxis.set_major_formatter(date_format)
+fig.autofmt_xdate()
+fig.tight_layout()
 
 
 
-# df['sentiment'] = df['sentiment'].rolling(int(len(df)/5)).mean()
-# df.plot('date', 'sentiment')
+df['sentiment'] = df['sentiment'].rolling(int(len(df)/5)).mean()
+df.plot('date', 'sentiment')
 
 
-# plt.show()
+plt.show()
 
 
 # article = ib.reqNewsArticle(latest.providerCode, latest.articleId)
