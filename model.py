b85391cb401faa1164c082d1c0c81dc7be0af39f
Andy Chen <42586375+anchen31@users.noreply.github.com>
2021-11-03 06:42:31 -0700

    huh

--- model.py
+++ model.py
@@ -9,13 +9,65 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM, BatchNormalization
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
+import pymysql
+pymysql.install_as_MySQLdb()
+import mysql.connector
+from mysql.connector import Error
 
-#Desgin
-#have the model train on previous days data and beforehand
+import config
 
-#Then use the model on ibpy file to fire a trade
+password = config.password
 
 
-#use the model every minute?
 
-#pull data out of the ibpy db to use into the model
+#pull the data and read it 
+
+try:
+    con = mysql.connector.connect(
+    host = 'localhost',
+    database='twitterdb', 
+    user='root', 
+    password = password)
+
+    cursor = con.cursor()
+    # create data db with full data set
+    query = "select * from ibpy"
+    cursor.execute(query)
+    # get all records
+    db = cursor.fetchall()
+
+    df = pd.DataFrame(db)
+
+except mysql.connector.Error as e:
+    print("Error reading data from MySQL table", e)
+
+    cursor.close()
+    con.close()
+
+df = df.set_axis(['date', 'open', 'high', 
+                    'low', 'close', 'volume', 
+                    'average', 'barCount', 'bb_bbm', 
+                    'bb_bbh', 'bb_bbl', 'VWAP', 
+                    'RSI', 'STsupp', 'STres', 
+                    'LTsupp', 'LTres', 'GLD', 
+                    'UVXY', 'SQQQ'], axis=1, inplace=False)
+
+
+train_dates = df['date']
+
+cols = list(df)[1:]
+
+df_for_training = df[cols].astype(float)
+
+scaler = StandardScaler()
+
+scaler = scaler.fit(df_for_training)
+
+df_for_training_scaled = scaler.transform(df_for_training)
+print(type(df_for_training_scaled))
+
+plt.plot(df_for_training_scaled)
+plt.show()
