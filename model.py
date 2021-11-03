98d0c420696805c4bc741f21cd02729e56de9ab6
Andy Chen <42586375+anchen31@users.noreply.github.com>
2021-11-03 11:11:21 -0700

    huh

--- model.py
+++ model.py
@@ -21,7 +21,6 @@ import config
 
 password = config.password
 
-scaler = preprocessing.MinMaxScaler()
 min_max_scaler = preprocessing.MinMaxScaler()
 
 def scaleColumns(df, cols_to_scale):
@@ -32,40 +31,44 @@ def scaleColumns(df, cols_to_scale):
 
 #pull the data and read it 
 
-try:
-    con = mysql.connector.connect(
-    host = 'localhost',
-    database='twitterdb', 
-    user='root', 
-    password = password)
+# try:
+#     con = mysql.connector.connect(
+#     host = 'localhost',
+#     database='twitterdb', 
+#     user='root', 
+#     password = password)
 
-    cursor = con.cursor()
-    # create data db with full data set
-    query = "select * from ibpy"
-    cursor.execute(query)
-    # get all records
-    db = cursor.fetchall()
+#     cursor = con.cursor()
+#     # create data db with full data set
+#     query = "select * from ibpy"
+#     cursor.execute(query)
+#     # get all records
+#     db = cursor.fetchall()
 
-    df = pd.DataFrame(db)
+#     df = pd.DataFrame(db)
 
-except mysql.connector.Error as e:
-    print("Error reading data from MySQL table", e)
+# except mysql.connector.Error as e:
+#     print("Error reading data from MySQL table", e)
 
-    cursor.close()
-    con.close()
+#     cursor.close()
+#     con.close()
 
-df = df.set_axis(['date', 'open', 'high', 
-                    'low', 'close', 'volume', 
-                    'average', 'barCount', 'bb_bbm', 
-                    'bb_bbh', 'bb_bbl', 'VWAP', 
-                    'RSI', 'STsupp', 'STres', 
-                    'LTsupp', 'LTres', 'GLD', 
-                    'UVXY', 'SQQQ'], axis=1, inplace=False)
+# df = df.set_axis(['date', 'open', 'high', 
+#                     'low', 'close', 'volume', 
+#                     'average', 'barCount', 'bb_bbm', 
+#                     'bb_bbh', 'bb_bbl', 'VWAP', 
+#                     'RSI', 'STsupp', 'STres', 
+#                     'LTsupp', 'LTres', 'GLD', 
+#                     'UVXY', 'SQQQ'], axis=1, inplace=False)
+
+df = pd.read_csv('data.csv')
 
 
 train_dates = df['date']
 
-cols = list(df)[1:]
+#cols = list(df)[1:]
+cols = list(df)[2:]
+print(cols)
 
 df_for_training = df[cols].astype(float)
 
@@ -78,14 +81,16 @@ scaled_df = scaleColumns(df_for_training,['open', 'high',
                     'LTsupp', 'LTres', 'GLD', 
                     'UVXY', 'SQQQ'])
 
-scaler = StandardScaler()
+# scaler = StandardScaler()
 
-scaler = scaler.fit(df_for_training)
+# scaler = scaler.fit(df_for_training)
 
-df_for_training_scaled = scaler.transform(df_for_training)
+# df_for_training_scaled = scaler.transform(df_for_training)
+# print(type(df_for_training_scaled))
 
 scaled_df.plot()
 plt.show()
+
 
 
 # plt.plot(scaled_df)
